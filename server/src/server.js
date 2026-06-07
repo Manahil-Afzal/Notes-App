65698dbd7b4a193a18a0e56dfe9df3c2364af125
Manahil Afzal <manahilafzal731@gmail.com>
2026-06-07 09:24:56 -0700

    Notes app update

--- server/src/server.js
+++ server/src/server.js
@@ -9,16 +9,13 @@ const app = express();
 
 connectDB();
 
-// app.use(cors());
 app.use(
   cors({
-    origin: [
-      "https://notes-app-front-orpin.vercel.app/"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+    origin: process.env.CLIENT_URL,
+    credentials: true,
   })
 );
+
 // Rich-editor content can be large; increase body size limits
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
